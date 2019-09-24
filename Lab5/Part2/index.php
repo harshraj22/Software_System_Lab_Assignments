71afd8d94908f55a79ae302e0cf09b0c4ea963b3
harshraj22 <harshraj22aug@gmail.com>
2019-09-24 19:49:00 +0530

    added index.php

---
+++ Lab5/Part2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<style type="text/css">
+		body{
+			position: absolute;
+			width: 300px;
+			height: 200px;
+			z-index: 15;
+			top: 45%;
+			left: 45%;
+			margin: -100px 0 0 -150px;
+		}
+	</style>
+</head>
+<body>
+
+	<form action="login.php" method="post">
+		<input type="text" name="input_box" value="eval">
+		<input type="password" name="pass_box" value="eva">
+		<button type="submit">Submit</button>
+	</form>
+
+</body>
+</html>
